bbdfbb37a7e685728a9539819afca8d9ae1eecf4
Harrier <harrierbax@gmail.com>
2024-04-12 15:40:25 +0200

    download every 0.5 finished

--- simpledownload.py
+++ simpledownload.py
@@ -1,5 +1,6 @@
 import os
 import paramiko
+import time as t
 
 def download_files_via_ssh(hostname, port, username, password, remote_folder, local_folder):
     # Create SSH client
@@ -9,13 +10,20 @@ def download_files_via_ssh(hostname, port, username, password, remote_folder, lo
     try:
         # Connect to the SSH server
         ssh_client.connect(hostname=hostname, port=port, username=username, password=password)
-
+    except Exception as e:
+        print(f"Error connect: {e}")
+    try:
         # Create SFTP client
         sftp_client = ssh_client.open_sftp()
-
+    except Exception as e:
+        print(f"Error open_sftp: {e}")
+    try:
         # Change directory to remote folder
         sftp_client.chdir(remote_folder)
+    except Exception as e:
+        print(f"Error chdir: {e}")
 
+    try:
         # List files in the remote folder
         files = sftp_client.listdir()
 
@@ -26,11 +34,11 @@ def download_files_via_ssh(hostname, port, username, password, remote_folder, lo
             sftp_client.get(remote_file_path, local_file_path)
             print(f"Downloaded: {file_name}")
 
+    except Exception as e:
+        print(f"Error download: {e}")
+
         # Close SFTP client
         sftp_client.close()
-
-    except Exception as e:
-        print(f"Error: {e}")
 
     finally:
         # Close SSH client
@@ -42,8 +50,11 @@ if __name__ == "__main__":
     port = 22  # Default SSH port
     username = "havar"
     password = "123"  # Enter your password here
-    remote_folder = "~/repositories/vx-firmware/build-lynx-mp1/bin"
-    local_folder = "/"  # Change this to your local folder path
+    # remote_folder = "~/repositories/vx-firmware/build-lynx-mp1/bin"
+    remote_folder = f"/home/{username}/repositories/vx-firmware/build-lynx-mp1/bin"
+    local_folder = "../../repositories/bin"  # Change this to your local folder path
 
-    download_files_via_ssh(hostname, port, username, password, remote_folder, local_folder)
+    while True:
+        download_files_via_ssh(hostname, port, username, password, remote_folder, local_folder)
+        t.sleep(0.5)
 
