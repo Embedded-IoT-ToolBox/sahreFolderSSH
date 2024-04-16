8ada3f68d02e5c0d0dcd59a22753a8f83b7c828e
Harrier <harrierbax@gmail.com>
2024-04-16 17:54:04 +0200

    downloader

--- downloader.py
+++ downloader.py
@@ -1,42 +1,57 @@
 import os
 import paramiko
 import time as t
+import json
+import argparse
 
-def download_files_via_ssh(hostname, port, username, password, remote_folder, local_folder):
+def clear_terminal():
+    # For Windows
+    if os.name == 'nt':
+        _ = os.system('cls')
+    # For Unix/Linux/MacOS
+    else:
+        _ = os.system('clear')
+
+def load_config(filename):
+    with open(filename, 'r') as f:
+        return json.load(f)
+
+def download_files_via_ssh(config):
     # Create SSH client
     ssh_client = paramiko.SSHClient()
     ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
     try:
         # Connect to the SSH server
-        print("> connecting to SSH server: ",end="")
-        ssh_client.connect(hostname=hostname, port=port, username=username, password=password)
+        print("> connecting to SSH server: ", end="")
+        ssh_client.connect(hostname=config['hostname'], port=config['port'], username=config['username'], password=config['password'])
     except Exception as e:
         print(f"Error: {e}")
     print("ok")
     try:
         # Create SFTP client
-        print("> Creating SFTP client: ",end="")
+        print("> Creating SFTP client: ", end="")
         sftp_client = ssh_client.open_sftp()
     except Exception as e:
         print(f"Error: {e}")
     print("ok")
     try:
         # Change directory to remote folder
-        sftp_client.chdir(remote_folder)
+        sftp_client.chdir(config['remote_folder'])
     except Exception as e:
         print(f"Error chdir: {e}")
 
-    print("> Start checking the files... ")
     while True:
+        clear_terminal()  # Clear the terminal screen
+        print("> Start checking the files... ")
         try:
             # List files in the remote folder
             files = sftp_client.listdir_attr()
 
             # Check each file for modification and download if modified
             for file_info in files:
-                remote_file_path = os.path.join(remote_folder, file_info.filename)
-                local_file_path = os.path.join(local_folder, file_info.filename)
+                remote_file_path = os.path.join(config['remote_folder'], file_info.filename)
+                local_file_path = os.path.join(config['local_folder'], file_info.filename)
 
                 # Check if the file is a .hex file
                 if file_info.filename.endswith('.hex'):
@@ -70,16 +85,41 @@ def download_files_via_ssh(hostname, port, username, password, remote_folder, lo
     #     # Close SSH client
     #     ssh_client.close()
 
+def find_config_file():
+    default_file = 'default.conf'
+    if os.path.exists(default_file):
+        return default_file
+
+    for file in os.listdir('.'):
+        if file.endswith('.conf'):
+            return file
+
+    return None
+
+def get_config_file():
+    default_file = find_config_file()
+    if default_file:
+        return default_file
+    else:
+        print("Error: No configuration file found in the current directory.")
+        filename = input("Please enter the path to the configuration file: ")
+        return filename
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Download files via SSH from a remote server.')
+    parser.add_argument('-c', '--config', help='Path to the configuration file')
+    parser.add_argument('-h', '--help', action='help', default=argparse.SUPPRESS, help='Show this help message and exit')
+    return parser.parse_args()
+
 # Example usage
 if __name__ == "__main__":
-    hostname = "192.168.2.169"
-    port = 22  # Default SSH port
-    username = "havar"
-    password = "123"  # Enter your password here
-    # remote_folder = "~/repositories/vx-firmware/build-lynx-mp1/bin"
-    remote_folder = f"/home/{username}/repositories/vx-firmware/build-lynx-mp1/bin"
-    local_folder = "../../repositories/bin"  # Change this to your local folder path
+    args = parse_arguments()
 
-    download_files_via_ssh(hostname, port, username, password, remote_folder, local_folder)
-        
+    if args.config:
+        config_file = args.config
+    else:
+        config_file = get_config_file()
 
+    config = load_config(config_file)
+
+    download_files_via_ssh(config)
