0b1d2fee70f2468bb01c62c362d78c61a4b3c533
Harrier <harrierbax@gmail.com>
2024-04-16 18:26:48 +0200

    several nodes

--- downloader.py
+++ downloader.py
@@ -1,8 +1,10 @@
+#!/usr/bin/env python3
 import os
 import paramiko
 import time as t
 import json
 import argparse
+import sys
 
 def clear_terminal():
     # For Windows
@@ -16,79 +18,71 @@ def load_config(filename):
     with open(filename, 'r') as f:
         return json.load(f)
 
-def download_files_via_ssh(config):
-    # Create SSH client
-    ssh_client = paramiko.SSHClient()
-    ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-
-    try:
-        # Connect to the SSH server
-        print("> connecting to SSH server: ", end="")
-        ssh_client.connect(hostname=config['hostname'], port=config['port'], username=config['username'], password=config['password'])
-    except Exception as e:
-        print(f"Error: {e}")
-    print("ok")
-    try:
-        # Create SFTP client
-        print("> Creating SFTP client: ", end="")
-        sftp_client = ssh_client.open_sftp()
-    except Exception as e:
-        print(f"Error: {e}")
-    print("ok")
-    try:
-        # Change directory to remote folder
-        sftp_client.chdir(config['remote_folder'])
-    except Exception as e:
-        print(f"Error chdir: {e}")
-
+def download_files_via_ssh(master_node, nodes):
     while True:
         clear_terminal()  # Clear the terminal screen
         print("> Start checking the files... ")
-        try:
-            # List files in the remote folder
-            files = sftp_client.listdir_attr()
+        for node in nodes:
+            # Create SSH client
+            ssh_client = paramiko.SSHClient()
+            ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-            # Check each file for modification and download if modified
-            for file_info in files:
-                remote_file_path = os.path.join(config['remote_folder'], file_info.filename)
-                local_file_path = os.path.join(config['local_folder'], file_info.filename)
+            try:
+                # Connect to the SSH server
+                print(f"> Connecting to SSH server ({node['ip']}): ", end="")
+                ssh_client.connect(hostname=node['ip'], port=node['port'], username=node['username'], password=node['password'])
+                print("OK")
 
-                # Check if the file is a .hex file
-                if file_info.filename.endswith('.hex'):
+                # Create SFTP client
+                sftp_client = ssh_client.open_sftp()
 
-                    # Check if local file exists
-                    if os.path.exists(local_file_path):
-                        # Get modification time of local file
-                        local_mtime = os.path.getmtime(local_file_path)
+                # Change directory to remote folder
+                sftp_client.chdir(node['directory'])
 
-                        # Get modification time of remote file
-                        remote_mtime = file_info.st_mtime
+                # List files in the remote folder
+                files = sftp_client.listdir_attr()
 
-                        # Compare modification times
-                        if local_mtime < remote_mtime:
-                            # Download file if remote file is newer
+                # Check each file for modification and download if modified
+                for file_info in files:
+                    remote_file_path = os.path.join(node['directory'], file_info.filename)
+                    local_file_path = os.path.join(master_node['directory'], file_info.filename)
+
+                    # Check if the file is a .hex file
+                    if file_info.filename.endswith('.hex'):
+
+                        # Check if local file exists
+                        if os.path.exists(local_file_path):
+                            # Get modification time of local file
+                            local_mtime = os.path.getmtime(local_file_path)
+
+                            # Get modification time of remote file
+                            remote_mtime = file_info.st_mtime
+
+                            # Compare modification times
+                            if local_mtime < remote_mtime:
+                                # Download file if remote file is newer
+                                sftp_client.get(remote_file_path, local_file_path)
+                                print(f"Downloaded: {file_info.filename}")
+                        else:
+                            # Download file if local file doesn't exist
                             sftp_client.get(remote_file_path, local_file_path)
                             print(f"Downloaded: {file_info.filename}")
-                    else:
-                        # Download file if local file doesn't exist
-                        sftp_client.get(remote_file_path, local_file_path)
-                        print(f"Downloaded: {file_info.filename}")
 
-        except Exception as e:
-            print(f"Error download: {e}")
-        t.sleep(0.5)
+                # Close SFTP client
+                sftp_client.close()
 
-    # # Close SFTP client
-    # sftp_client.close()
+                # Close SSH client
+                ssh_client.close()
 
-    # finally:
-    #     # Close SSH client
-    #     ssh_client.close()
+            except Exception as e:
+                print(f"Error: {e}")
+
+            t.sleep(0.5)
 
 def find_config_file():
-    default_file = 'default.conf'
-    if os.path.exists(default_file):
-        return default_file
+    config_file = 'nodes.conf'
+    if os.path.exists(config_file):
+        return config_file
 
     for file in os.listdir('.'):
         if file.endswith('.conf'):
@@ -96,30 +90,42 @@ def find_config_file():
 
     return None
 
+def check_config_file(filename):
+    try:
+        with open(filename, 'r') as f:
+            json.load(f)
+        return True
+    except (json.JSONDecodeError, FileNotFoundError):
+        return False
+
 def get_config_file():
-    default_file = find_config_file()
-    if default_file:
-        return default_file
+    config_file = find_config_file()
+    if config_file:
+        return config_file
     else:
         print("Error: No configuration file found in the current directory.")
-        filename = input("Please enter the path to the configuration file: ")
-        return filename
+        sys.exit(1)
 
 def parse_arguments():
-    parser = argparse.ArgumentParser(description='Download files via SSH from a remote server.')
+    parser = argparse.ArgumentParser(description='Download files via SSH from remote servers.')
     parser.add_argument('-c', '--config', help='Path to the configuration file')
-    parser.add_argument('-h', '--help', action='help', default=argparse.SUPPRESS, help='Show this help message and exit')
-    return parser.parse_args()
+    parser.add_argument('-v', '--version', action='version', version='%(prog)s 1.0')
+    args = parser.parse_args()
+
+    if args.config:
+        if not check_config_file(args.config):
+            print("Error: The provided configuration file is not valid or does not exist.")
+            sys.exit(1)
+
+    return args
 
 # Example usage
 if __name__ == "__main__":
     args = parse_arguments()
 
-    if args.config:
-        config_file = args.config
-    else:
-        config_file = get_config_file()
-
+    config_file = args.config if args.config else get_config_file()
     config = load_config(config_file)
 
-    download_files_via_ssh(config)
+    master_node = config['master']
+
+    download_files_via_ssh(master_node, config['nodes'])
